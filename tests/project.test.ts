6c48a752cb51d3d5d8555a7000fbba7c9c1d3d88
rohitaryal <erucix@gmail.com>
2025-05-27 13:33:51 +0530

    tabs changed

--- tests/project.test.ts
+++ tests/project.test.ts
@@ -2,42 +2,43 @@ import Whisk from '../src';
 import { expect, test } from 'bun:test';
 
 const whisk = new Whisk({
-    cookie: process.env.COOKIE || "INVALID_COOKIE",
+  cookie: process.env.COOKIE || "INVALID_COOKIE",
 });
 
 const projects = await whisk.getProjectHistory(12);
 
 test("Getting new project id.", async () => {
-    const project = await whisk.getNewProjectId("Funny images");
+  const project = await whisk.getNewProjectId("Funny images");
 
-    expect(project.Ok).toBeDefined();
-    expect(project.Err).toBeUndefined();
-    // '30' as length is unknown, mine was 36
-    expect(project.Ok!.length > 30).toBe(true);
+  expect(project.Ok).toBeDefined();
+  expect(project.Err).toBeUndefined();
+  // '30' as length is unknown, mine was 36
+  expect(project.Ok!.length > 30).toBe(true);
 });
 
 test("Getting project history.", async () => {
-    const projects = await whisk.getProjectHistory(10);
+  const projects = await whisk.getProjectHistory(10);
 
-    expect(projects.Ok).toBeDefined()
-    expect(projects.Err).toBeUndefined();
-    expect(Array.isArray(projects.Ok)).toBe(true);
+  expect(projects.Ok).toBeDefined()
+  expect(projects.Err).toBeUndefined();
+  expect(Array.isArray(projects.Ok)).toBe(true);
 });
 
 test("Get project history with long count limit.", async () => {
-    const projects = await whisk.getProjectHistory(10000000000);
+  const projects = await whisk.getProjectHistory(10000000000);
 
-    expect(projects.Err).toBeDefined()
-    expect(projects.Ok).toBeUndefined();
+  expect(projects.Err).toBeDefined()
+  expect(projects.Ok).toBeUndefined();
 });
 
+// Test if users has any projects
 test.if((projects.Ok?.length || 0) > 0)("Get project contents", async () => {
-    const projectId = projects.Ok![0]!.name;
+  const projectId = projects.Ok![0]!.name;
 
-    const projectContent = await whisk.getProjectContent(projectId);
-    expect(projectContent.Err).toBeUndefined();
-    expect(projectContent.Ok).toBeDefined();
+  const projectContent = await whisk.getProjectContent(projectId);
+  expect(projectContent.Err).toBeUndefined();
+  expect(projectContent.Ok).toBeDefined();
 
-    expect(projectContent.Ok!.length > 0).toBe(true);
-    expect(projectContent.Ok![0]?.name).toBeDefined()
+  expect(projectContent.Ok!.length > 0).toBe(true);
+  expect(projectContent.Ok![0]?.name).toBeDefined()
 })
