629a0b6ed40ba6ee43523c945e70a3a6f0b25f75
rohitaryal <erucix@gmail.com>
2025-05-27 18:42:05 +0530

    add refinement test

--- tests/generation.test.ts
+++ tests/generation.test.ts
@@ -52,8 +52,6 @@ test("Image generation without project id", async () => {
     expect(imagePanel.generatedImages[0]!.encodedImage).toBeDefined();
 }, 30000);
 
-
-
 test("Image generation with image aspect ratio", async () => {
     const prompt: Prompt = {
         aspectRatio: "IMAGE_ASPECT_RATIO_SQUARE",
@@ -71,3 +69,35 @@ test("Image generation with image aspect ratio", async () => {
     expect(imagePanel.generatedImages.length).toBeGreaterThan(0);
     expect(imagePanel.generatedImages[0]!.encodedImage).toBeDefined();
 }, 30000);
+
+test("Image refinement", async () => {
+    const prompt: Prompt = {
+        prompt: "A person who always forgets to write prompts",
+    }
+    const image = await whisk.generateImage(prompt);
+    expect(image.Err).toBeUndefined();
+    expect(image.Ok).toBeDefined();
+    expect(image.Ok!.imagePanels).toBeInstanceOf(Array);
+    expect(image.Ok!.imagePanels).toHaveLength(1);
+    const imagePanel = image.Ok!.imagePanels[0]!;
+    expect(imagePanel.generatedImages).toBeInstanceOf(Array);
+    expect(imagePanel.generatedImages.length).toBeGreaterThan(0);
+    expect(imagePanel.generatedImages[0]!.encodedImage).toBeDefined();
+    const encodedImage = imagePanel.generatedImages[0]!.encodedImage;
+    const refinedImage = await whisk.refineImage({
+        existingPrompt: imagePanel.generatedImages[0]!.prompt,
+        newRefinement: "The person is wearing a hat",
+        base64image: encodedImage,
+        imageId: imagePanel.generatedImages[0]!.mediaGenerationId,
+    });
+    expect(refinedImage.Err).toBeUndefined();
+    expect(refinedImage.Ok).toBeDefined();
+    expect(refinedImage.Ok!.imagePanels).toBeInstanceOf(Array);
+    expect(refinedImage.Ok!.imagePanels).toHaveLength(1);
+    const refinedImagePanel = refinedImage.Ok!.imagePanels[0]!;
+    expect(refinedImagePanel.generatedImages).toBeInstanceOf(Array);
+    expect(refinedImagePanel.generatedImages.length).toBeGreaterThan(0);
+    expect(refinedImagePanel.generatedImages[0]!.encodedImage).toBeDefined();
+    const refinedEncodedImage = refinedImagePanel.generatedImages[0]!.encodedImage;
+    expect(refinedEncodedImage).toBeDefined();
+}, 40000);
