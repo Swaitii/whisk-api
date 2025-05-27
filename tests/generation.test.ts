75e07304d20fa3c724cb71091d69ed5018947ab6
rohitaryal <erucix@gmail.com>
2025-05-27 15:42:48 +0530

    more tests

---
+++ tests/generation.test.ts
@@ -0,0 +1,73 @@
+import Whisk from '../src';
+import { expect, test, beforeEach } from 'bun:test';
+import type { Prompt } from '../src/global.types';
+
+const whisk = new Whisk({
+    cookie: process.env.COOKIE || "INVALID_COOKIE",
+});
+
+test("Image generation", async () => {
+    const project = await whisk.getNewProjectId("Sample Project");
+
+    expect(project.Err).toBeUndefined();
+    expect(project.Ok).toBeDefined();
+
+    const prompt: Prompt = {
+        projectId: project.Ok!,
+        prompt: "A person who always forgets to write prompts",
+    };
+
+    const image = await whisk.generateImage(prompt);
+
+    expect(image.Err).toBeUndefined();
+    expect(image.Ok).toBeDefined();
+
+    expect(image.Ok!.imagePanels).toBeInstanceOf(Array);
+    expect(image.Ok!.imagePanels).toHaveLength(1);
+
+    const imagePanel = image.Ok!.imagePanels[0]!;
+    expect(imagePanel.generatedImages).toBeInstanceOf(Array);
+    expect(imagePanel.generatedImages.length).toBeGreaterThan(0);
+    expect(imagePanel.generatedImages[0]!.encodedImage).toBeDefined();
+}, 30000);
+
+
+
+test("Image generation without project id", async () => {
+    const prompt: Prompt = {
+        prompt: "A person who always forgets to write prompts",
+    };
+
+    const image = await whisk.generateImage(prompt);
+
+    expect(image.Ok).toBeDefined();
+    expect(image.Err).toBeUndefined();
+
+    expect(image.Ok!.imagePanels).toBeInstanceOf(Array);
+    expect(image.Ok!.imagePanels).toHaveLength(1);
+
+    const imagePanel = image.Ok!.imagePanels[0]!;
+    expect(imagePanel.generatedImages).toBeInstanceOf(Array);
+    expect(imagePanel.generatedImages.length).toBeGreaterThan(0);
+    expect(imagePanel.generatedImages[0]!.encodedImage).toBeDefined();
+}, 30000);
+
+
+
+test("Image generation with image aspect ratio", async () => {
+    const prompt: Prompt = {
+        aspectRatio: "IMAGE_ASPECT_RATIO_SQUARE",
+        prompt: "A person who always forgets to write prompts",
+    };
+
+
+    const image = await whisk.generateImage(prompt);
+
+    expect(image.Ok!.imagePanels).toBeInstanceOf(Array);
+    expect(image.Ok!.imagePanels).toHaveLength(1);
+
+    const imagePanel = image.Ok!.imagePanels[0]!;
+    expect(imagePanel.generatedImages).toBeInstanceOf(Array);
+    expect(imagePanel.generatedImages.length).toBeGreaterThan(0);
+    expect(imagePanel.generatedImages[0]!.encodedImage).toBeDefined();
+}, 30000);
